c7af13e2d21dbf844460eae976fc25230e35ebc5
Ryan Cuthbertson <100254988+rcuthbertson25@users.noreply.github.com>
2024-07-06 23:12:32 -0400

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -12,7 +12,9 @@ const Footer = () => {
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
                     <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
+                    <a href="https://www.linkedin.com/in/ryan-cuthbertson-61a980281/" target="_blank" rel="noopener noreferrer">
+                      <img src={assets.linkedin_icon} alt="LinkedIn" />
+                    </a>
                 </div>
             </div>
             <div className="footer-content-center">
